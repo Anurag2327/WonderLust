c3f88d54236c68c64b92a76d915336d6810ebec2
Anurag Pandey <152264132+Anurag2327@users.noreply.github.com>
2026-07-12 12:06:30 +0530

    Update app.js

--- app.js
+++ app.js
@@ -64,9 +64,9 @@ const sessionOptions = {
     }
 }
 
-// app.get("/",(req,res) => {
-//     res.render("./listings/home.ejs")
-// });
+app.get("/",(req,res) => {
+    res.render("./listings/home.ejs")
+});
 
 
 app.use(session(sessionOptions));
