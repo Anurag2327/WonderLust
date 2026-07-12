871e9582e0df45d82a1811b5c34b54d27bf605d6
Anurag Pandey <152264132+Anurag2327@users.noreply.github.com>
2026-07-12 13:31:16 +0530

    Update app.js

--- app.js
+++ app.js
@@ -64,10 +64,6 @@ const sessionOptions = {
     }
 }
 
-<<<<<<< HEAD
-=======
-
->>>>>>> 55a36fa (add category filter and search functionality)
 app.use(session(sessionOptions));
 app.use(flash());
 
@@ -87,10 +83,7 @@ app.use((req,res,next) =>{
 app.get("/",(req,res) => {
     res.redirect("./listings")
 });
-<<<<<<< HEAD
-=======
 
->>>>>>> 55a36fa (add category filter and search functionality)
 app.use("/listings", listingRouter);
 app.use("/listings/:id/reviews", reviewRouter);
 app.use("/", userRouter);
